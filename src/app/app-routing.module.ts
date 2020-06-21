19f53c693e26e69fb80553c885c50deabeb5ddd4
hemantCC <49364193+hemantCC@users.noreply.github.com>
2020-06-21 19:21:07 +0530

    InitialCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'Home',
+    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
